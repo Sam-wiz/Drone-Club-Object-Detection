e07c2ae2a9d05ce0d311edc201b61ad00d212858
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-04-14 17:25:24 +0530

    Added few files

--- sam_odm.py
+++ sam_odm.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow as tf
+import onnxruntime as rt
 from PIL import Image
 
 # Define a mapping from class IDs to class names (modify as needed)
@@ -9,13 +9,8 @@ class_names = {
     # ... add more class names as needed
 }
 
-# Load the TFLite model
-interpreter = tf.lite.Interpreter(model_path="detect.tflite")
-interpreter.allocate_tensors()
-
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+# Load the ONNX model
+sess = rt.InferenceSession("model.onnx")
 
 # Open the camera
 cap = Image.open(0)
@@ -32,19 +27,11 @@ while True:
     # Preprocess the frame for the model
     frame = frame.convert('RGB')
     frame = frame.resize((640, 480))
-    input_tensor = tf.convert_to_tensor(np.array(frame), dtype=tf.uint8)
+    input_tensor = np.array(frame).astype('float32')
     input_tensor = input_tensor[tf.newaxis, ...]
 
-    # Set the tensor to point to the input data to be inferred
-    interpreter.set_tensor(input_details[0]['index'], input_tensor)
-
-    # Run inference
-    interpreter.invoke()
-
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[0]['index'])  # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[1]['index'])  # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[2]['index'])  # Confidence of detected objects
+    # Run object detection
+    boxes, scores, classes = sess.run(None, {sess.get_inputs()[0].name: input_tensor})
 
     # Draw bounding boxes and labels on the frame
     for box, score, class_id in zip(boxes[0], scores[0], classes[0]):
